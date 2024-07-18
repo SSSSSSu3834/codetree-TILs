897fc3e520602c321a15074ef65b4c43032faee1
Hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2024-07-18 17:25:21 +0900

    Title: 줄 세우기, Time: 165ms, Memory: 25MB, Status: Passed - CodeTree

--- 240718/줄 세우기/line-up-students.py
+++ 240718/줄 세우기/line-up-students.py
@@ -13,7 +13,7 @@ for i in range(n):
     t, w = map(int, input().split())
     people.append(Person(t, w, i+1))
 
-people.sort(key = lambda x: (-x.tall, -x.weight, -x.num))    
+people.sort(key = lambda x: (-x.tall, -x.weight, x.num))    
 
 for i in people:
     print(i.tall, i.weight, i.num)
