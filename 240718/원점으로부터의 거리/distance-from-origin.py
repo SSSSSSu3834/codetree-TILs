77c64e412b8bc7226793bb83130cdcd912358c72
Hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2024-07-18 18:13:36 +0900

    Title: 원점으로부터의 거리, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240718/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+# 원점에서 짧은 거리부터 출력(오름차순 출력)
+spots= []
+
+for i in range(2):
+    x, y = map(int, input().split())
+    spots.append((x, y, i+1))
+
+spots.sort(key = lambda x: (abs(0-x[0])+ abs(0-x[1]), x[2]))
+
+for i in spots:
+    print(i[2])
