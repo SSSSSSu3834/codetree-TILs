f67193d3daaf7d9459415ca2cfabe970e66ebf0e
Hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2024-07-18 16:38:26 +0900

    Title: 총점 비교, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240718/총점 비교/compare-total-points.py
@@ -0,0 +1,24 @@
+n = int(input())
+scores = []
+
+class Score:
+    def __init__(self, name, kor, eng, math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+
+for _ in range(n):
+    data = input().split()
+    n = data[0]
+    k = int(data[1])
+    e = int(data[2])
+    m = int(data[3])
+
+    scores.append(Score(n,k,e,m))
+
+scores.sort(key = lambda x : x.kor+ x.eng+ x.math)
+
+for i in scores:
+    print(i.name, i.kor, i.eng, i.math)
