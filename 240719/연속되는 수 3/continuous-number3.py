9dfe0b96c2e921014d591f067123283b1b118fef
Hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2024-07-19 17:04:09 +0900

    Title: 연속되는 수 3, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240719/연속되는 수 3/continuous-number3.py
+++ 240719/연속되는 수 3/continuous-number3.py
@@ -10,15 +10,22 @@ for _ in range(n):
 
 for i in range(n):
     
-    if (num[i]<0 and num[i-1]<0) or (num[i]>0 and num[i-1]>0):
-        count+=1
-        if result < count:
-            result = count
     # 만약 배열이 1개 라면 count값이 곧 result
-    else:
+    if i == 0:
         count = 1
         if result <= count:
             result = count
+    
+    # 부호 같을 때 count올리고 result에 저장
+    elif (num[i]<0 and num[i-1]<0) or (num[i]>0 and num[i-1]>0):
+        count+=1
+        if result < count:
+            result = count
+
+    # 부호 다를 때 count값 초기화함
+    else:
+        count = 1
+
     
 
 print(result)
