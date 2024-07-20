b8706cb10f84d990572e43c995194973f1cc30a2
Hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2024-07-21 00:59:41 +0900

    Title: 선두를 지켜라, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240721/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,51 @@
+n, m = map(int, input().split())
+
+a= []
+b=[]
+La = []
+Lb = []
+result = []
+count = 0
+
+for _ in range(n):
+    v, t = map(int, input().split())
+    a.append((v,t))
+for _ in range(m):
+    v, t = map(int, input().split())
+    b.append((v,t))
+
+totalTime = 0
+
+for i in a:
+    totalTime += i[1]
+
+# a, b 초기값
+ax = 0
+bx = 0
+
+for i in a:
+    for j in range(i[1]):
+        ax = ax + i[0]
+        La.append(ax)
+
+for i in b:
+    for j in range(i[1]):
+        bx = bx + i[0]
+        Lb.append(bx)
+
+
+for i in range(1, len(La)+1):
+
+    if La[i-1] > Lb[i-1] and La[i] >= Lb[i]:
+        result.append("A")
+    else:
+        result.append("B")
+
+for i in range(len(result)):
+    if i==0:
+        count = 0
+    elif result[i] != result[i-1]:
+        count+=1
+    
+
+print(count)
