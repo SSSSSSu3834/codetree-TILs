7c8eb0d557821bafcbf93476ea1375e5ec8371ba
Hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2024-07-26 13:34:00 +0900

    Title: 행복한 수열의 개수, Time: 163ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240726/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,37 @@
+# 행복한 수열 = 연속하여 m개 이상의 동일한 원소가 나오는 순간이 존재하는 수열
+n,m = map(int, input().split())
+num = []
+count=1
+result = 0 
+
+for _ in range(n):
+    num.append(list(map(int, input().split())))
+
+
+for i in range(n):
+    for j in range(n):
+        # 열에대한 순열
+        if j !=0 and num[i][j] == num[i][j-1]:
+            count+=1
+        else:
+            count = 1
+
+    
+    if count >= m:
+        result +=1
+        count = 1
+
+    # 행에다한 수열
+    for k in range(n):
+        if k !=0 and num[k][i] == num[k-1][i]:
+            count+=1
+        else:
+            count = 1
+
+    if count >= m:
+        result += 1
+        count = 1
+
+    
+
+print(result)
