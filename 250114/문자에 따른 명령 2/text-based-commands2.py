a0a7bdea6c193613083fe6d1fb9035d7d92aae19
Hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2025-01-14 18:08:37 +0900

    Title: 문자에 따른 명령 2, Time: 82ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250114/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,27 @@
+dirs = input()
+
+# Write your code here!
+x,y = 0,0
+dx, dy = [0,1,0,-1],[1,0,-1,0]
+
+# 북쪽부터 시계 방향으로 dir 증가
+dir = 0 
+
+for i in list(dirs):
+    if i=="R":
+        dir=(dir + 1)% 4
+    elif i == "L":
+        dir = (dir + 3)% 4
+    else:
+        nx, ny = x + dx[dir], y + dy[dir]
+
+
+print(nx, ny)
+        
+    
+
+
+
+
+
+
